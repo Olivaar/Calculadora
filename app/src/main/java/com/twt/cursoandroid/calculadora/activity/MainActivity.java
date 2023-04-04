1a4afbc1f33ac8f00801eef1c8a7f222280a1eb8
Olivar Neemias Moises Chone Tamele <mocolivarbaloi@gmail.com>
2023-04-04 15:09:41 -0300

    Criação de métodos de click para botões.

--- app/src/main/java/com/twt/cursoandroid/calculadora/activity/MainActivity.java
+++ app/src/main/java/com/twt/cursoandroid/calculadora/activity/MainActivity.java
@@ -15,7 +15,10 @@ public class MainActivity extends AppCompatActivity {
             buttonSeis, buttonSete, buttonOito, buttonNove, buttonIgual, buttonSoma, buttonSubtracao,
             buttonMultiplicacao, buttonDiv, buttonVirgula, buttonMaisMenos, buttonParenteses, buttonC;
     private TextView textVisor, textResposta;
-    private String number1;
+    private String number1 = "";
+    private String number2 = "";
+    private String operador;
+    private Double num1, num2, resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +49,143 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //Testar condição após criar métodos de calculo
+    public void clickSoma(View view){
+        if (resultado != null){
+            operador = "+";
+            String visor = textVisor.getText().toString();
+            textVisor.setText(visor+ " + ");
+        }else{
+            operador = "+";
+            String visor = textVisor.getText().toString();
+            textVisor.setText(visor+ " + ");
+        }
+    }
+
+    public void clickSubtracao(View view){
+        operador = "-";
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ " - ");
+    }
+
+    public void clickMultiplicacao(View view){
+        operador = "*";
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ " x ");
+    }
+
+    public void clickDivisao(View view){
+        operador = "/";
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ " / ");
+    }
     //Método para testes de onClick
-//    public void clickZero(View view){
-//        number1 += "0";
-//        String teste = textVisor.getText().toString();
-//        textVisor.setText(teste+ "0");
-//    }
+    public void clickZero(View view){
+
+        if (operador != null){
+            number2 += "0";
+        }else{
+            number1 += "0";
+        }
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ "0");
+        System.out.println(number1);
+        System.out.println(number2);
+    }
+
+    public void clickUm(View view){
+        if (operador != null){
+            number2 += "1";
+        }else{
+            number1 += "1";
+        }
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ "1");
+    }
+
+    public void clickDois(View view){
+        if (operador != null){
+            number2 += "2";
+        }else{
+            number1 += "2";
+        }
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ "2");
+    }
+
+    public void clickTres(View view){
+        if (operador != null){
+            number2 += "3";
+        }else{
+            number1 += "3";
+        }
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ "3");
+    }
+
+    public void clickQuatro(View view){
+        if (operador != null){
+            number2 += "4";
+        }else{
+            number1 += "4";
+        }
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ "4");
+    }
+
+    public void clickCinco(View view){
+        if (operador != null){
+            number2 += "5";
+        }else{
+            number1 += "5";
+        }
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ "5");
+    }
+
+    public void clickSeis(View view){
+        if (operador != null){
+            number2 += "6";
+        }else{
+            number1 += "6";
+        }
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ "6");
+    }
+
+    public void clickSete(View view){
+        if (operador != null){
+            number2 += "7";
+        }else{
+            number1 += "7";
+        }
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ "7");
+    }
+
+    public void clickOito(View view){
+        if (operador != null){
+            number2 += "8";
+        }else{
+            number1 += "8";
+        }
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ "8");
+    }
+
+    public void clickNove(View view){
+        if (operador != null){
+            number2 += "9";
+        }else{
+            number1 += "9";
+        }
+        String visor = textVisor.getText().toString();
+        textVisor.setText(visor+ "9");
+    }
+
+    public void clickC(View view){
+        textVisor.setText("");
+        number1 = "";
+        number2 = "";
+    }
 }
